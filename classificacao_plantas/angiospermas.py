d3f9041e2eb5d126b9e7506eb80baab5eb481f77
Grazi427 <sousagraziele16@gmail.com>
2026-01-20 21:30:49 -0300

    Add files via upload

--- classificacao_plantas/angiospermas.py
+++ classificacao_plantas/angiospermas.py
@@ -3,7 +3,3 @@ from plantas import Planta
 class Angiosperma(Planta):
     def obter_tipo(self):
         return "Angiosperma (Plantas com flores/frutos)"
-
-    def descrever_biologia(self):
-        # POLIMORFISMO: Cada classe responde de forma diferente a este método
-        return "Planta complexa com vasos, sementes, flores e frutos."
